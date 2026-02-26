1137bd9f5d7e2fdf9baf23ce041c928621a65db0
Ali Baba Azimi <azimi.ali@tum.de>
2026-02-26 13:56:13 +0100

    add transparent background to the base model

--- app/components/BaseModal.vue
+++ app/components/BaseModal.vue
@@ -7,7 +7,8 @@
       >
         <!-- Backdrop -->
         <div
-          class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
+          class="fixed inset-0 transition-opacity"
+          style="background: rgba(0, 0, 0, 0.7); backdrop-filter: blur(2px);"
           @click="$emit('update:modelValue', false)"
         ></div>
 
